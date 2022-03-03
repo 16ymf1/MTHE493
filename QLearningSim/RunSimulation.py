b5605b0faaf80b73189b4c3889eacc57e679fb99
Luca Trombetta <lucatrombetta@Lucas-MacBook-Pro.local>
2022-03-03 15:09:59 -0500

    update

--- QLearningSim/RunSimulation.py
+++ QLearningSim/RunSimulation.py
@@ -5,12 +5,12 @@ import numpy as np
 ORDER_RATE = 0.15
 LEARNING_RATE = 0.8
 DISCOUNT_RATE = 0.95
-NUM_EPISODES = 100000
+NUM_EPISODES = 50000
 NUM_TIMESTEPS = 288
 
 
 restuarants = [(0,0),(9,9),(4,3),(7,2),(3,8)]
-couriers = [[(0,0), 1], [(2,3), 2]]
+couriers = [[(0,0), 1], [(2,3), 1]]
 sim = SimulationEnvironment(10, restuarants, couriers)
 model = QLearningModel(LEARNING_RATE, DISCOUNT_RATE, NUM_EPISODES, NUM_TIMESTEPS, sim, ORDER_RATE)
 model.run_sim()
